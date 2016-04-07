e3325df99ffcc85a4aa76883c49afa315ef9fbd2
Osh Pilaf <faizalmansor@gmail.com>
2016-04-07 18:30:37 +0800

    Add new AccessRule for multi roles

---
+++ common/components/AccessRule.php
@@ -0,0 +1,34 @@
+<?php
+ 
+namespace common\components;
+
+use common\models\User;
+
+class AccessRule extends \yii\filters\AccessRule
+{
+    /**
+     * @inheritdoc
+     */
+    protected function matchRole($user)
+    {
+        if (empty($this->roles)) {
+            return true;
+        }
+        foreach ($this->roles as $role) {
+            if ($role === '?') {
+                if ($user->getIsGuest()) {
+                    return true;
+                }
+            } elseif ($role === '@') {
+                if (!$user->getIsGuest()) {
+                    return true;
+                }
+            // Check if the user is logged in, and the roles match
+            } elseif (!$user->getIsGuest() && in_array($role, $user->identity->roles)) {
+                return true;
+            }
+        }
+ 
+        return false;
+    }
+}
