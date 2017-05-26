9d42b9f0a2d6f207945ea1101429f721815b382c
Osh Pilaf <faizalmansor@gmail.com>
2017-05-26 16:35:27 +0800

    update for languages & gitignore in common config

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -124,4 +124,36 @@ class SiteController extends Controller
 
         return $this->goHome();
     }
+    
+    public function actionAll()
+    {
+        // Function to get all controllers & actions
+        $controllerlist = [];
+        if ($handle = opendir('../backend/controllers')) {
+            while (false !== ($file = readdir($handle))) {
+                if ($file != "." && $file != ".." && substr($file, strrpos($file, '.') - 10) == 'Controller.php') {
+                    $controllerlist[] = $file;
+                }
+            }
+            closedir($handle);
+        }
+        asort($controllerlist);
+        $fulllist = [];
+        foreach ($controllerlist as $controller):
+            $handle = fopen('../backend/controllers/' . $controller, "r");
+            if ($handle) {
+                while (($line = fgets($handle)) !== false) {
+                    if (preg_match('/public function action(.*?)\(/', $line, $display)):
+                        if (strlen($display[1]) > 2):
+                            $fulllist[substr($controller, 0, -4)][] = strtolower($display[1]);
+                        endif;
+                    endif;
+                }
+            }
+            fclose($handle);
+        endforeach;
+        echo '<pre>';
+        print_r ($fulllist);
+        echo '</pre>';
+    }
 }
--- common/config/main.php
+++ common/config/main.php
@@ -1,9 +1,32 @@
 <?php
+Yii::setAlias('@approot', realpath(dirname(__FILE__).'/../../'));
+
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'language'=>'en',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
+        ],
+        'i18n' => [
+            'translations' => [
+                'app*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@approot/messages',
+                ],
+                'yii*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@approot/messages',
+                ],
+            ],
+        ],
+        'urlManager' => [
+    		    'class' => 'codemix\localeurls\UrlManager',
+            'languages' => ['en', 'my'],
+            //'enablePrettyUrl' => true,
+            //'showScriptName' => false,
+            //'rules' => [
+            //],
         ],
     ],
 ];
